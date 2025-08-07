d8017b98358105a9f5daf8aa40fbb39c0f822b73
boat.suka <boat.suka@gmail.com>
2025-08-07 11:17:09 +0700

    chore(backend): modify server.ts to use new port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express, { type Request, type Response } from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express with TypeScript!');
